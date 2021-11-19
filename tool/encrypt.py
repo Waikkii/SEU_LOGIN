be4b78fb768f4a6c395460d4e227993310353392
Waikiki <50266226+Waikkii@users.noreply.github.com>
2021-11-19 16:09:43 +0800

    Update encrypt.py

--- tool/encrypt.py
+++ tool/encrypt.py
@@ -1,5 +1,8 @@
 import js2py
 context = js2py.EvalJs()
+import logging
+logging.basicConfig(level=logging.INFO, format='%(message)s')
+logger = logging.getLogger(__name__)
 
 with open("./tool/encrypt.js") as f:
     js_content = f.read()
@@ -7,5 +10,5 @@ with open("./tool/encrypt.js") as f:
 def encryptAES(data, salt):
     context.execute(js_content)
     result = context.encryptAES(data, salt)
-    print("Encrypted passwd:", result)
+    logger.info("Encrypted passwd:" + result)
     return result
